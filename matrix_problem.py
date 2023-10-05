efe5ba21462f0810d1d85d86bb2975b27a06d014
Mohamedfariq <mohamedfariq2326@gmail.com>
2023-10-05 21:10:00 +0530

    To find row and coloumn of an element in matrix

--- matrix_problem.py
+++ matrix_problem.py
@@ -2,3 +2,11 @@ from numpy import *
 
 matrix=array([[1,3,7],[5,6,9],[8,2,0]])
 print(matrix)
+
+to_find=int(input("Enter an element : "))
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        if(to_find==matrix[i][j]):
+            print("Row : ",i+1)
+            print("Coloumn : ",j+1)
+            break
