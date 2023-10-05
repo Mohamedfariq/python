7efc9d4471a16d6afef40489d33092a177ee6928
Mohamedfariq <mohamedfariq2326@gmail.com>
2023-10-05 20:45:46 +0530

    work with arithmetic operators

---
+++ arithmetic_expression.py
@@ -0,0 +1,12 @@
+def expression(a,b,c):
+    if a+b==c:
+        print(True)
+    elif a-b==c:
+        print(True)
+    elif a*b==c:
+        print(True)
+    elif a/b==c:
+        print(True)
+    else:
+        print(True)
+expression(2,3,-1)
