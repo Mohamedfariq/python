acbad12e629fa6a205da89244c207124ce2a52ae
Mohamedfariq <mohamedfariq2326@gmail.com>
2023-10-15 16:13:52 +0530

    Take set of strings as input from user if the length of each set is not divisible by 2 than combine that set with very next set

---
+++ string_separation.py
@@ -0,0 +1,16 @@
+string="( () ((()()())) (()) ()"
+x=string.split()
+print(x)
+i=0
+while i<len(x):
+    if len(x[i])%2!=0:
+        try:
+            print(x[i]+x[i+1],end=' ')
+        except IndexError:
+            print()
+    if len(x[i])%2==0:
+        try:
+            print(x[i+1],end=' ')
+        except IndexError:
+            print()
+    i=i+1
