c773c040e3cedbbf04e02eaf1090547a383d8e1a
Mohamedfariq <mohamedfariq2326@gmail.com>
2023-10-22 21:35:26 +0530

    To find second minimum in a list without using inbuilt functions

---
+++ secondminimum.py
@@ -0,0 +1,16 @@
+list1=[]
+n=int(input("Enter the number of elements : "))
+for i in range(n):
+    x=input('Enter the element : ')
+    list1.append(x)
+minimum=list1[0]
+secondmin=list1[1]
+for i in list1[1:]:
+    if i>minimum:
+        secondmin=minimum
+        minimum=i
+    elif i>secondmin:
+        secondmin=i      
+print(secondmin)
+    
+    
