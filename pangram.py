a7f837e87409df2d611888663370d0379a3a7b2f
Mohamedfariq <mohamedfariq2326@gmail.com>
2023-11-08 23:06:23 +0530

    To Check whether a given string is pangram or not

--- pangram.py
+++ pangram.py
@@ -1,12 +1,11 @@
-str1=sorted(input().lower())
 list1=[chr(i) for i in range(97,123)]
+str1 = list(input("Enter a string :").lower())
 count=0
-for i in str1:
-    if i==' ':
-        count+=1
-for i in range(count):   
-    str1.remove(' ')
-if str1==list1:
-    print("It is a pangram")
+for i in str1 :
+    if i not in str1:
+        count +=1
+        break 
+if count==0:
+    print("It is a panagram")
 else:
-    print("It is not a pangram")
+    print("It is not a panagram")
