43b1758f1d700aa5baf3278cd2bb5e45101aa9aa
Mohamedfariq <mohamedfariq2326@gmail.com>
2023-11-22 21:32:00 +0530

    Binary search using functions

---
+++ binarysearch.py
@@ -0,0 +1,25 @@
+def binarysearch(l,key):
+    low=0
+    high=len(l)-1
+    while low<=high:
+        mid=(low+high)//2
+        if l[mid]==key:
+            print("Element found")
+            break
+        else:
+            if l[mid]<key:
+                low=mid
+            if l[mid]>key:
+                high=mid
+            else:
+                print("Element not found")
+                break
+    return " "
+l=[]
+n=int(input("Enter the number of elements :"))
+for i in range(n):
+    x=int(input("Enter an element to be added :"))
+    l.append(x)
+key=int(input("Enter an element to be searched :"))
+print(binarysearch(l,key)) 
+
